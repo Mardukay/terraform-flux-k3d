84f1b37084def2f0c7021b8d13b255df73abc6d8
Serhii Tsilko <mardukay@gmail.com>
2023-12-26 20:19:15 +0200

    add dependency

--- main.tf
+++ main.tf
@@ -13,9 +13,20 @@ module "tls_private_key" {
 }
 
 module "k3d_cluster" {
-    source = "github.com/Mardukay/terraform-flux-k3d"
+    source = "github.com/Mardukay/tf-kubernetes-cluster"
     name = var.cluster_name
     server = var.server_count
     agent = var.agent_count 
+}
+
+module "flux_bootstrap" {
+  source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap"
+  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
+  private_key       = module.tls_private_key.private_key_pem
+  config_path       = module.k3d_cluster.kubeconfig
+  github_token      = var.github_token
+  depends_on = [
+    git_repo
+  ]
 }
 
