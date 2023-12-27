376cc380dd6f3f64396bcf0bcff9e273cab176c2
Serhii Tsilko <mardukay@gmail.com>
2023-12-27 13:51:59 +0200

    add flux resource

--- main.tf
+++ main.tf
@@ -1,32 +1,53 @@
+#create github repository for flux
 module "git_repo" {
-    source = "github.com/den-vasyliev/tf-github-repository"
-    owner = var.github_owner
-    token = var.github_token
-    repository = var.repository_name
-    visibility = var.repository_visibility
-    public_key_openssh = module.tls_private_key.public_key_openssh
-    public_key_openssh_title = flux
+  source                   = "github.com/den-vasyliev/tf-github-repository"
+  github_owner             = var.GITHUB_OWNER
+  github_token             = var.GITHUB_TOKEN
+  repository_name          = var.FLUX_GITHUB_REPO
+  public_key_openssh       = module.tls_private_key.public_key_openssh
+  repository_visibility    = var.repository_visibility
+  public_key_openssh_title = "flux"
 }
 
+#create ssh key for repository
 module "tls_private_key" {
   source = "github.com/den-vasyliev/tf-hashicorp-tls-keys"
 }
 
+#create kubernetes cluster for flux in local k3d
 module "k3d_cluster" {
-    source = "github.com/Mardukay/tf-kubernetes-cluster"
-    name = var.cluster_name
-    server = var.server_count
-    agent = var.agent_count 
+  source           = "github.com/Mardukay/tf-kubernetes-cluster"
+  k3d_cluster_name = var.k3d_cluster_name
+  server_count     = var.server_count
+  agent_count      = var.agent_count
 }
 
-module "flux_bootstrap" {
-  source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap"
-  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
-  private_key       = module.tls_private_key.private_key_pem
-  config_path       = module.k3d_cluster.kubeconfig
-  github_token      = var.github_token
+  # create kubeconfig file and store path to var.kubeconfig
+resource "null_resource" "kubeconfig" {
+  for_each = toset(var.k3d_cluster_name)
   depends_on = [
-    git_repo
+    module.k3d_cluster
   ]
+  provisioner "local-exec" {
+    command = "export TF_VAR_kubeconfig=$(k3d kubeconfig write ${each.key})"
+  }
 }
 
+#deploy flux to cluster and connect to github repository
+provider "flux" {
+  kubernetes = {
+    config_path = var.kubeconfig
+  }
+  git = {
+    url = "https://github.com/${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}.git"
+    http = {
+      username = "git"
+      password = var.GITHUB_TOKEN
+    }
+  }
+}
+
+resource "flux_bootstrap_git" "this" {
+  path       = var.target_path
+  depends_on = [module.git_repo, module.k3d_cluster, module.tls_private_key]
+}
--- variables.tf
+++ variables.tf
@@ -1,29 +1,32 @@
 variable "k3d_cluster_name" {
-  default = "flux"
-  type = string
+  default = ["flux"]
+  type    = list(string)
+  description = "Cluster names in list"
 }
 
 variable "server_count" {
   default = 1
   type    = number
+  description = "Cluster servers count"
 }
 
 variable "agent_count" {
   default = 0
   type    = number
+  description = "Cluster nodes count"
 }
 
-variable "github_owner" {
+variable "GITHUB_OWNER" {
   type        = string
   description = "The GitHub owner"
 }
 
-variable "github_token" {
+variable "GITHUB_TOKEN" {
   type        = string
   description = "GitHub personal access token"
 }
 
-variable "repository_name" {
+variable "FLUX_GITHUB_REPO" {
   type        = string
   default     = "flux-gitops"
   description = "GitHub repository"
@@ -35,5 +38,15 @@ variable "repository_visibility" {
   description = "The visibility of the GitOps repository"
 }
 
+variable "target_path" {
+  type        = string
+  default     = "clusters"
+  description = "Flux manifests subdirectory"
+}
 
+variable "kubeconfig" {
+  default = ""
+  type    = string
+  description = "Kubeconfig path"
+}
 
